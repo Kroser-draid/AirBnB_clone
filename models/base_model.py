e1a2c41576ca3449738845870657fb354cf0a4d8
Kroser-draid <kingdomskroser@gmail.com>
2024-05-19 01:28:55 +0100

    try again

--- models/base_model.py
+++ models/base_model.py
@@ -6,21 +6,35 @@ import datetime
 
 
 class BaseModel:
-    """_summary_
+    """Base classe for other classes
     """
 
     def __init__(self):
+        """initialize the instance attributes
+        """
         self.id = str(uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return f"[<{self.__class__.__name__}>] (<{self.id}>) <{self.__dict__}>"
+        """str method
+
+        Returns:
+            string: name of instance, id, and dict
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """method that update data
+        """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        """method that return dict
+
+        Returns:
+            str: isoformat of datetime of created and updated
+        """
         result = self.__dict__.copy()
         result['__class__'] = self.__class__.__name__
         if 'created_at' in result:
