761be1b6a3849067d12933dd5512e7d15ba01af7
houikhalfan <h.ouikhalfan@gmail.com>
2024-05-21 12:35:30 +0100

    d9

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -9,7 +9,6 @@ from datetime import datetime
 from time import sleep
 from models.amenity import Amenity
 
-
 class TestAmenity_instantiation(unittest.TestCase):
     """
     Unittests for testing instantiation of the Amenity class.
