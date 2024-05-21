da3b88f5e4de6001ae08998ae97be66976cc50a0
houikhalfan <h.ouikhalfan@gmail.com>
2024-05-21 12:34:10 +0100

    h6

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -69,7 +69,6 @@ class TestBasemodel(unittest.TestCase):
         self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
         self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
 
-
     def test_str(self):
         """
         Test for string representation
